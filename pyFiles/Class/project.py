e2b3bdb8bac49dbd8c7324a2009c4261dcc889f7
Hellmesss <gdemonet@yahoo.fr>
2022-11-07 17:15:19 -0500

    reinstall modules and del sql connection

--- pyFiles/Class/project.py
+++ pyFiles/Class/project.py
@@ -8,24 +8,3 @@ class Project:
         self.name = name
         self.time = time
         self.date = date
-
-try:
-    connection = mysql.connector.connect(host='localhost',
-                                         database='Electronics',
-                                         user='pynative',
-                                         password='pynative@#29')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_Info)
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
-
-except Error as e:
-    print("Error while connecting to MySQL", e)
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL connection is closed")
